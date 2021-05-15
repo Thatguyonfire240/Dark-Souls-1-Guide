bff54966cac399814847eee2b3b22110ea266978
Thatguyonfire240 <marqsandstrom@gmail.com>
2021-05-15 02:24:52 -0700

    Imported Dict_Init function into the main guide

--- Dict_Initialization.py
+++ Dict_Initialization.py
@@ -1,81 +1,81 @@
 from Guide import Trophy_guide
-import csv
-filename="Sorceries_Wisdom_of_a_Sage.csv"
-Sorceries_Wisdom_of_a_Sage = {}
-with open(filename, 'r') as data:
-    for line in csv.DictReader(data):
-        Sorceries_Wisdom_of_a_Sage = line
-        print(Sorceries_Wisdom_of_a_Sage)
+def Dict_Init():
+    import csv
+    filename="Sorceries_Wisdom_of_a_Sage.csv"
+    Sorceries_Wisdom_of_a_Sage = {}
+    with open(filename, 'r') as data:
+        for line in csv.DictReader(data):
+            Sorceries_Wisdom_of_a_Sage = line
+            print(Sorceries_Wisdom_of_a_Sage)
+    filename="Knights_Honor_Sorted_by_Categories_Boss_Soul_Weapons.csv"
+    Knights_Honor_Sorted_by_Categories_Boss_Soul_Weapons = {}
+    with open(filename, 'r') as data:
+        for line in csv.DictReader(data):
+            Knights_Honor_Sorted_by_Categories_Boss_Soul_Weapons = line
+            print(Knights_Honor_Sorted_by_Categories_Boss_Soul_Weapons)
+            
+    filename="guide.csv"
+    Guide = {}
+    with open(filename, 'r') as data:
+        for line in csv.DictReader(data):
+            Guide = line
 
-filename="Knights_Honor_Sorted_by_Categories_Boss_Soul_Weapons.csv"
-Knights_Honor_Sorted_by_Categories_Boss_Soul_Weapons = {}
-with open(filename, 'r') as data:
-    for line in csv.DictReader(data):
-        Knights_Honor_Sorted_by_Categories_Boss_Soul_Weapons = line
-        print(Knights_Honor_Sorted_by_Categories_Boss_Soul_Weapons)
+    filename="Knights_Honor_Sorted_by_Categories_Guaranteed_Loot.csv"
+    Knights_Honor_Sorted_by_Categories_Guaranteed_Loot = {}
+    with open(filename, 'r') as data:
+        for line in csv.DictReader(data):
+            Knights_Honor_Sorted_by_Categories_Guaranteed_Loot = line
 
-filename="guide.csv"
-Guide = {}
-with open(filename, 'r') as data:
-    for line in csv.DictReader(data):
-        Guide = line
+    filename="Knights_Honor_Sorted_by_Categories_Tails.csv"
+    Knights_Honor_Sorted_by_Categories_Tails = {}
+    with open(filename, 'r') as data:
+        for line in csv.DictReader(data):
+            Knights_Honor_Sorted_by_Categories_Tails = line
 
-filename="Knights_Honor_Sorted_by_Categories_Guaranteed_Loot.csv"
-Knights_Honor_Sorted_by_Categories_Guaranteed_Loot = {}
-with open(filename, 'r') as data:
-    for line in csv.DictReader(data):
-        Knights_Honor_Sorted_by_Categories_Guaranteed_Loot = line
+    filename="Knights_Honor_Sorted_by_Categories_Weapon_Drops.csv"
+    Knights_Honor_Sorted_by_Categories_Weapon_Drops = {}
+    with open(filename, 'r') as data:
+        for line in csv.DictReader(data):
+            Knights_Honor_Sorted_by_Categories_Weapon_Drops = line
 
-filename="Knights_Honor_Sorted_by_Categories_Tails.csv"
-Knights_Honor_Sorted_by_Categories_Tails = {}
-with open(filename, 'r') as data:
-    for line in csv.DictReader(data):
-        Knights_Honor_Sorted_by_Categories_Tails = line
+    filename="Knights_Honor_Sorted_by_Inventory_Order.csv"
+    Knights_Honor_Sorted_by_Inventory_Order = {}
+    with open(filename, 'r') as data:
+        for line in csv.DictReader(data):
+            Knights_Honor_Sorted_by_Inventory_Order = line
 
-filename="Knights_Honor_Sorted_by_Categories_Weapon_Drops.csv"
-Knights_Honor_Sorted_by_Categories_Weapon_Drops = {}
-with open(filename, 'r') as data:
-    for line in csv.DictReader(data):
-        Knights_Honor_Sorted_by_Categories_Weapon_Drops = line
+    filename="Miracles_Prayer_of_a_Maiden.csv"
+    Miracles_Prayer_of_a_Maiden = {}
+    with open(filename, 'r') as data:
+        for line in csv.DictReader(data):
+            Miracles_Prayer_of_a_Maiden = line
 
-filename="Knights_Honor_Sorted_by_Inventory_Order.csv"
-Knights_Honor_Sorted_by_Inventory_Order = {}
-with open(filename, 'r') as data:
-    for line in csv.DictReader(data):
-        Knights_Honor_Sorted_by_Inventory_Order = line
+    filename="Pyromancies_Bond_of_a_Pyromancer.csv"
+    Pyromancies_Bond_of_a_Pyromancer = {}
+    with open(filename, 'r') as data:
+        for line in csv.DictReader(data):
+            Pyromancies_Bond_of_a_Pyromancer = line
 
-filename="Miracles_Prayer_of_a_Maiden.csv"
-Miracles_Prayer_of_a_Maiden = {}
-with open(filename, 'r') as data:
-    for line in csv.DictReader(data):
-        Miracles_Prayer_of_a_Maiden = line
+    filename="Weapon_Upgrades_Andre_of_Astora.csv"
+    Weapon_Upgrades_Andre_of_Astora = {}
+    with open(filename, 'r') as data:
+        for line in csv.DictReader(data):
+            Weapon_Upgrades_Andre_of_Astora = line
 
-filename="Pyromancies_Bond_of_a_Pyromancer.csv"
-Pyromancies_Bond_of_a_Pyromancer = {}
-with open(filename, 'r') as data:
-    for line in csv.DictReader(data):
-        Pyromancies_Bond_of_a_Pyromancer = line
+    filename="Weapon_Upgrades_Giant_Blacksmith.csv"
+    Weapon_Upgrades_Giant_Blacksmith = {}
+    with open(filename, 'r') as data:
+        for line in csv.DictReader(data):
+            Weapon_Upgrades_Giant_Blacksmith = line
 
-filename="Weapon_Upgrades_Andre_of_Astora.csv"
-Weapon_Upgrades_Andre_of_Astora = {}
-with open(filename, 'r') as data:
-    for line in csv.DictReader(data):
-        Weapon_Upgrades_Andre_of_Astora = line
+    filename="Weapon_Upgrades_Rickert_of_Vinheim.csv"
+    Weapon_Upgrades_Rickert_of_Vinheim = {}
+    with open(filename, 'r') as data:
+        for line in csv.DictReader(data):
+            Weapon_Upgrades_Rickert_of_Vinheim = line
 
-filename="Weapon_Upgrades_Giant_Blacksmith.csv"
-Weapon_Upgrades_Giant_Blacksmith = {}
-with open(filename, 'r') as data:
-    for line in csv.DictReader(data):
-        Weapon_Upgrades_Giant_Blacksmith = line
-
-filename="Weapon_Upgrades_Rickert_of_Vinheim.csv"
-Weapon_Upgrades_Rickert_of_Vinheim = {}
-with open(filename, 'r') as data:
-    for line in csv.DictReader(data):
-        Weapon_Upgrades_Rickert_of_Vinheim = line
-
-filename="Weapon_Upgrades_Vamos.csv"
-Weapon_Upgrades_Vamos = {}
-with open(filename, 'r') as data:
-    for line in csv.DictReader(data):
-        Weapon_Upgrades_Vamos = line
+    filename="Weapon_Upgrades_Vamos.csv"
+    Weapon_Upgrades_Vamos = {}
+    with open(filename, 'r') as data:
+        for line in csv.DictReader(data):
+            Weapon_Upgrades_Vamos = line
--- Guide.py
+++ Guide.py
@@ -1,3 +1,6 @@
+#import dictionary initalization function, then run it to initialize the dictionaries
+from Dict_Initialization import Dict_Init
+Dict_Init()
 def Main_menu():
     print("""Hello! Welcome to the interactive Dark Souls 1 Guide!
     Please select an option to continue!
